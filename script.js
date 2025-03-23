cfa241aeecca42fb3a450d17b48b58cfbfd93cc4
Tom Leclercq <tomleclercq2006@gmail.com>
2025-03-23 12:14:09 +0100

    ajout d'animations GSAP pour les sections sec1 et sec2

--- script.js
+++ script.js
@@ -1,57 +1,44 @@
-const lenis = new Lenis({
-    duration: 0.04,  // Ajuste la durée pour un scroll plus naturel (1 = rapide, 2 = lent)
-    smooth: true,   // Active le smooth scroll
-    smoothTouch: false, // Désactive le smooth sur mobile (sinon ça peut être bizarre)
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Courbe d'animation plus fluide
-  });
-  
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-  
-  requestAnimationFrame(raf);
-  
-
-// Vérification que GSAP fonctionne
-console.log("GSAP chargé :", gsap);
-console.log("ScrollTrigger chargé :", ScrollTrigger);
-
-// Active ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
 
-// Animation de la section hero
-gsap.from(".hero h2, .hero p", {
-    opacity: 0,
-    y: 50,
-    duration: 1.2,
-    ease: "power2.out",
-    delay: 0.5
+const box1 = document.querySelector(".sec1 .box1");
+const box2 = document.querySelector(".sec1 .box2");
+
+gsap.to(box1, {
+  xPercent: 500,
+  duration: 2,
+  scrollTrigger: {
+    trigger: box1,
+    toggleActions: "restart reverse play reverse",
+    start: "top 40%",
+    end: "bottom 30%",
+    markers: false,
+    scrub: 1
+  }
 });
 
-// Animation de la section "projects"
-gsap.from(".projects h2, .project-card", {
-    scrollTrigger: {
-        trigger: ".projects",
-        start: "top 80%",
-        toggleActions: "play none none reverse",
-    },
-    opacity: 0,
-    y: 50,
-    duration: 1.2,
-    stagger: 0.3, // Décale l’apparition des cartes
-    ease: "power2.out"
+gsap.to(box2, {
+  xPercent: 500,
+  duration: 2,
+  scrollTrigger: {
+    trigger: box1,
+    toggleActions: "restart reverse play reverse",
+    start: "top 40%",
+    end: "bottom 30%",
+    scrub: 3
+  }
 });
 
-// Animation de la section "contact"
-gsap.from(".contact h2, .contact p", {
-    scrollTrigger: {
-        trigger: ".contact",
-        start: "top 80%",
-        toggleActions: "play none none reverse",
+gsap.from(".sec2 .box", {
+    scrollTrigger : {
+        trigger : ".sec2 .box",
+        toggleActions : "restart none none reset",
+        start : "top 80%",
+        markers : false,
     },
-    opacity: 0,
-    y: 50,
-    duration: 1.2,
-    ease: "power2.out"
-});
+    y : 100,
+    opacity : 0,
+    scale : 0,
+    ease : "elastic(0.4,0.15)",
+    duration : 1,
+    stagger : 0.1,
+})
